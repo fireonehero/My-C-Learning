6774e4a720b8b54fc9790adeba3133e7d9340353
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2025-05-29 23:14:04 +0000

    Create main.cpp

---
+++ ContactList/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+struct Contact {
+    std::string Name;
+    std::string Phone;
+    std::string Email;
+};
+
+void addContact(){
+    
+}
+
+void deleteContact(){
+
+}
+
+void listContact(){
+
+}
+
+void searchContact(){
+
+}
+
+bool saveAndQuit(){
+    
+}
+
+
+int main(){
+
+
+
+    return 0;
+}
