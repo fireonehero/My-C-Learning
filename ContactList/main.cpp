f60c7827e87776292007739b5020a58fff66f200
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2025-05-31 18:49:20 +0000

    Update main.cpp

--- ContactList/main.cpp
+++ ContactList/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <limits>
+#include <algorithm>
+#include <iomanip>
 
 struct Contact {
     std::string Name;
@@ -8,38 +12,78 @@ struct Contact {
     std::string Email;
 };
 
-void addContact(){
-    
+std::string contactFile = "contacts.txt";
+
+void addContact(std::vector<Contact>& contactBook){
+    std::string fullName;
+    std::string phoneNumber;
+    std::string email;
+
+    std::cout << "Enter full name: ";
+    std::getline(std::cin, fullName);
+
+    std::cout << "Enter phone: ";
+    std::getline(std::cin, phoneNumber);
+
+    std::cout << "Enter email: ";
+    std::getline(std::cin, email);
+
+    Contact newContact{fullName, phoneNumber, email};
+    contactBook.push_back(newContact);
 }
 
-void deleteContact(){
+void deleteContact(std::vector<Contact>& contactBook){
+    std::string name;
+
 
 }
 
-void listContact(){
+void listContact(std::vector<Contact>& contactBook){
+    std::string name;
+
+    if(contactBook.empty()){
+        std::cout << "No contacts in your address book.\n";
+        return;
+    }
+
+    for(const auto& contacts : contactBook){
+        std::cout << contacts.Name << "," << contacts.Phone << "," << contacts.Email << std::endl;
+    }
 
 }
 
-void searchContact(){
+std::vector<Contact> searchContact(const std::vector<Contact>& contactBook){
+    std::string nameSearch;
+    std::vector<Contact> searchResults;
 
+    std::cout << "Enter name to search: ";
+    std::getline(std::cin, nameSearch);
+
+    std::copy_if(contactBook.begin(), contactBook.end(), std::back_inserter(searchResults), [&nameSearch](const Contact& c){return c.Name.find(nameSearch) != std::string::npos;});
+
+    return searchResults;
 }
 
 bool saveAndQuit(){
+
+
+
 
     return false;
 }
 
 void menu(){
-    std::cout << "### Contact Manager ###";
-    std::cout << "1) Add a contact";
-    std::cout << "2) Delete a contact";
-    std::cout << "3) List all contacts";
-    std::cout << "4) Search by name";
-    std::cout << "5) Save & quit";
+    std::cout << "### Contact Manager ###\n";
+    std::cout << "1) Add a contact\n";
+    std::cout << "2) Delete a contact\n";
+    std::cout << "3) List all contacts\n";
+    std::cout << "4) Search by name\n";
+    std::cout << "5) Save & quit\n";
     std::cout << "Enter choice (1-5): ";
 }
 
 int main(){
+    std::cout << std::fixed << std::setprecision(2);
     int userChoice;
     bool running = true;
     std::vector<Contact> contactBook;
@@ -47,27 +91,46 @@ int main(){
     while (running) {
 
         menu();
-
-        std::cout << "Enter choice: ";
         std::cin >> userChoice;
 
         switch (userChoice){
             case 1:
-                addContact();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                addContact(contactBook);
                 break;
             case 2:
-                deleteContact();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                deleteContact(contactBook);
                 break;
             case 3:
-                listContact();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                listContact(contactBook);
                 break;
             case 4:
-                searchContact();
-                break;
+                {
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::vector<Contact> foundContacts = searchContact(contactBook);
+
+                    if(foundContacts.empty()){
+                        std::cout << "No contacts in your address book.\n";
+                    } else {
+                        std::cout << "\n--- Search Results ---\n";
+                        for(const auto& contacts : foundContacts){
+                            std::cout << "Name: " << contacts.Name << "\n";
+                            std::cout << "Phone Number: " << contacts.Phone << "\n";
+                            std::cout << "Email: " << contacts.Email << "\n";
+                            std::cout << "----------------------\n";
+                        }
+                    }
+                    break;
+                }
             case 5:
                 running = saveAndQuit();
+                break;
+            default:
+                std::cout << "Error: Invalid choice. Please enter a number from 1 to 5.";
         }
     }
-    
+
     return 0;
 }
