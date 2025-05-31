cb05d11a66d44768ac2ef8214bb57f2df7b8f773
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2025-05-31 00:22:49 +0000

    Update main.cpp

--- ContactList/main.cpp
+++ ContactList/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 struct Contact {
     std::string Name;
@@ -24,13 +25,49 @@ void searchContact(){
 }
 
 bool saveAndQuit(){
-    
+
+    return false;
 }
 
+void menu(){
+    std::cout << "### Contact Manager ###";
+    std::cout << "1) Add a contact";
+    std::cout << "2) Delete a contact";
+    std::cout << "3) List all contacts";
+    std::cout << "4) Search by name";
+    std::cout << "5) Save & quit";
+    std::cout << "Enter choice (1-5): ";
+}
 
 int main(){
+    int userChoice;
+    bool running = true;
+    std::vector<Contact> contactBook;
 
+    while (running) {
 
+        menu();
 
+        std::cout << "Enter choice: ";
+        std::cin >> userChoice;
+
+        switch (userChoice){
+            case 1:
+                addContact();
+                break;
+            case 2:
+                deleteContact();
+                break;
+            case 3:
+                listContact();
+                break;
+            case 4:
+                searchContact();
+                break;
+            case 5:
+                running = saveAndQuit();
+        }
+    }
+    
     return 0;
 }
