e9da8dae05fc654ffbdc0508a51b892704952cd1
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2025-06-13 19:26:14 +0000

    Create main.cpp

---
+++ ConwaysGameOfLife/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+void clearScreen() {
+    std::cout << "\x1B[2J\x1B[H";
+}
+
+void printBoard(){
+    clearScreen();
+
+}
+
+
+int main() {
+    const int N = 20;
+    char board[N][N];
+
+
+
+    clearScreen();
+
+
+    return 0;
+}
